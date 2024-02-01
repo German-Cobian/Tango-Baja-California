684656aa97b8f06494f1174b4be33c7b7b38322d
German Renato Cobian <germancobian@hotmail.com>
2024-02-01 13:44:31 -0800

    Create Admin controller, routes, index view and views/layouts/admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   root "home#index"
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "admin" => "admin#index"
+
 end
