d6f80c02a7f867ea8ef6b8bd59f87b1a4ad0d29f
German Renato Cobian <germancobian@hotmail.com>
2024-02-14 17:59:35 -0800

    Link app with Stripe + Develop functionality of checkouts_controller + Test connection of checkout button to Stripe

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,6 +1,37 @@
 class CheckoutsController < ApplicationController
 
   def create
-    puts "Hit create"
+    stripe_secret_key = Rails.application.credentials.dig(:stripe, :secret_key)
+    Stripe.api_key = stripe_secret_key
+    cart = params[:cart]
+    line_items = cart.map do |item|
+      product = Product.find(item["id"])
+
+      { 
+        quantity: item["quantity"].to_i,
+        price_data: { 
+          product_data: {
+            name: item["name"],
+            metadata: { product_id: product.id } 
+          },
+          currency: "usd",
+          unit_amount: item["price"].to_i
+        }
+      }
+    end
+    
+    puts "line_items: #{line_items}"
+
+    session = Stripe::Checkout::Session.create(
+      mode: "payment",
+      line_items: line_items,
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel",
+      shipping_address_collection: { 
+        allowed_countries: ['US', 'CA']
+      }
+    )
+    render json: { url: session.url }
   end
+
 end
